8bf7a50bfecf17d7d27521a1f5a46809720c4bc7
pkim14 <phoebe19kim@gmail.com>
2025-06-12 09:22:31 -0400

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,302 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.awt.Rectangle;
+
+public class Card {
+    private String suit;
+    private String value;
+    private String imageFileName;
+    private String backImageFileName;
+    private boolean show;
+    private BufferedImage image;
+    private Rectangle cardBox;
+    private boolean highlight;
+
+    public Card(String suit, String value) {
+        this.suit = suit;
+        this.value = value;
+        this.imageFileName = "images/card_" + suit + "_" + value + ".png";
+        this.show = true;
+        this.backImageFileName = "images/card_back.png";
+        this.image = readImage();
+        this.cardBox = new Rectangle(-100, -100, image.getWidth(), image.getHeight());
+        this.highlight = false;
+    }
+
+    public static boolean canEliminate(ArrayList<Card> hand) {
+        int numHighlightedCards = 0;
+        boolean canEliminate = false;
+
+        for (int i = 0; i < hand.size(); i++) {
+            if (hand.get(i).getHighlight()) {
+                numHighlightedCards++;
+            }
+        }
+        int totalSum = 0;
+        if (numHighlightedCards == 2) {
+            for (int i = 0; i < hand.size(); i++) {
+                if (hand.get(i).getHighlight()) {
+                    String val = hand.get(i).getValue();
+                    if (val.equalsIgnoreCase("A")) {
+                        totalSum++;
+                    } else if (val.equals("01")) {
+                        totalSum++;
+                    } else if (val.equals("02")) {
+                        totalSum += 2;
+                    } else if (val.equals("03")) {
+                        totalSum += 3;
+                    } else if (val.equals("04")) {
+                        totalSum += 4;
+                    } else if (val.equals("05")) {
+                        totalSum += 5;
+                    } else if (val.equals("06")) {
+                        totalSum += 6;
+                    } else if (val.equals("07")) {
+                        totalSum += 7;
+                    } else if (val.equals("08")) {
+                        totalSum += 8;
+                    } else if (val.equals("09")) {
+                        totalSum += 9;
+                    } else if (val.equals("10")) {
+                        totalSum += 10;
+                    }
+                }
+            }
+            if (totalSum == 1) {
+                canEliminate = true;
+            }
+        }
+        if (numHighlightedCards == 3) {
+            int jacks = 0;
+            int queens = 0;
+            int kings = 0;
+
+            for (int i = 0; i < hand.size(); i++) {
+                if (hand.get(i).getHighlight()) {
+                    String value = hand.get(i).getValue();
+                    if (value.equalsIgnoreCase("J")) {
+                        jacks++;
+                    } else if (value.equalsIgnoreCase("Q")) {
+                        queens++;
+                    } else if (value.equalsIgnoreCase("K")) {
+                        kings++;
+                    }
+                }
+            }
+            if (jacks == 1 && queens == 1 && kings == 1) {
+                canEliminate = true;
+            }
+        }
+        return canEliminate;
+    }
+
+    public static void replaceCard(ArrayList<Card> deck, ArrayList<Card> hand, int index) {
+        ArrayList<Card> temp = new ArrayList<>();
+        for (int i = 0; i < hand.size(); i++) {
+            for (int x = 0; x < deck.size(); x++) {
+                if (!deck.get(x).getImageFileName().equalsIgnoreCase(hand.get(i).getImageFileName())) {
+                    temp.add(deck.get(x));
+                }
+            }
+        }
+        int r = (int) (Math.random() * temp.size());
+        hand.set(index, temp.get(r));
+
+        return hand;
+    }
+
+    public static boolean movesLeft(ArrayList<Card> hand) {
+        boolean movesLeft = false;
+
+        for (int i = 0; i < hand.size(); i++) {
+            String str = hand.get(i).getValue();
+            int num = 0;
+
+            if (!(str.equals("A") || str.equals("K") || str.equals("Q") || str.equals("J") || str.equals("10"))) {
+                String[] parts = str.split("");
+                str = parts[1];
+                num = Integer.parseInt(str);
+            }
+            else if (str.equals("A")) {
+                num = 1;
+            }
+            else if (str.equals("10")) {
+                num = 10;
+            }
+            for (int j = 0; j < hand.size(); j++) {
+                String temp = hand.get(j).getValue();
+                int num2 = 0;
+
+                if (!(temp.equals("A") || temp.equals("K") || temp.equals("Q") || temp.equals("J") || temp.equals("10"))) {
+                    String[] parts2 = temp.split("");
+                    str = parts2[1];
+                    num2 = Integer.parseInt(str);
+                }
+                else if (temp.equals("A")) {
+                    num2 = 1;
+                }
+                else if (str.equals("10")) {
+                    num2 = 10;
+                }
+                if (num + num2 == 11) {
+                    movesLeft = true;
+                    System.out.println("NUM1: " + num2);
+                    System.out.println("NUM2: " + num2);
+                    System.out.println("EQUALS 11");
+                }
+            }
+        }
+
+        int jack = 0;
+        int queen = 0;
+        int king = 0;
+
+        for (Card c : hand) {
+            if (c.getValue().equalsIgnoreCase("J")) {
+                jack ++;
+            }
+            else if (c.getValue().equalsIgnoreCase("Q")) {
+                queen ++;
+            }
+            else if (c.getValue().equalsIgnoreCase("K")) {
+                king ++;
+            }
+        }
+        if (jack > 0 && queen > 0 && king > 0) {
+            movesLeft = true;
+            System.out.println("EQUALS JQK");
+        }
+        return movesLeft;
+    }
+
+    public Rectangle getCardBox() {
+        return cardBox;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setRectangleLocation(int x, int y) {
+        cardBox.setLocation(x, y);
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getImageFileName() {
+        return imageFileName;
+    }
+
+    public String toString() {
+        return suit + " " + value;
+    }
+
+    public void flipCard() {
+        show = !show;
+        this.image = readImage();
+    }
+
+    public void flipHighlight() {
+        highlight = !highlight;
+    }
+
+    public boolean getHighlight() {
+        return highlight;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public BufferedImage readImage() {
+        try {
+            BufferedImage image;
+            if (show) {
+                image = ImageIO.read(new File(imageFileName));
+            }
+            else {
+                image = ImageIO.read(new File(backImageFileName));
+            }
+            return image;
+        }
+        catch (IOException e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public static ArrayList<Card> buildDeck() {
+        ArrayList<Card> deck = new ArrayList<Card>();
+        String[] suits = {"clubs", "diamonds", "hearts", "spades"};
+        String[] values = {"02", "03", "04", "05", "06", "07", "08", "09", "10", "A", "J", "K", "Q"};
+        for (String s : suits) {
+            for (String v : values) {
+                Card c = new Card(s, v);
+                deck.add(c);
+            }
+        }
+        return deck;
+    }
+
+    public static ArrayList<Card> buildHand() {
+        ArrayList<Card> deck = Card.buildDeck();
+        ArrayList<Card> hand = new ArrayList<Card>();
+        for (int i = 0; i < 5; i++) {
+            int r = (int)(Math.random()*deck.size());
+            Card c = deck.remove(r);
+            hand.add(c);
+        }
+        return hand;
+    }
+
+    public static ArrayList<Card> checkDeck(ArrayList<Card> deck, ArrayList<Card> hand) {
+        if (Card.canEliminate(hand)) {
+            for (int h = 0; h < hand.size(); h++) {
+                if (hand.get(h).getHighlight()) {
+                    String handName = hand.get(h).getImageFileName();
+                    for (int i = 0; i < deck.size(); i++) {
+                        String deckName = deck.get(i).getImageFileName();
+                        if (handName.equals(deckName)) {
+                            deck.remove(i);
+                            i--;
+                        }
+                    }
+                }
+            }
+        }
+        return deck;
+    }
+
+    public static ArrayList<Card> replacedCard(ArrayList<Card> deck, ArrayList<Card> hand, int index) {
+        ArrayList<Card> temp = new ArrayList<>();
+
+        for (int i = 0; i < hand.size(); i++) {
+            for (int x = 0; x < deck.size(); x++) {
+                if (!deck.get(x).getImageFileName().equalsIgnoreCase(hand.get(i).getImageFileName()) {
+                    temp.add(deck.get(x));
+                }
+            }
+        }
+        int r = (int)(Math.random() * temp.size());
+        hand.set(index, temp.get(r));
+
+        return hand;
+    }
+}
+
+    private String getValue() {
+    }
+
+    public static ArrayList<Card> buildDeck() {
+    }
+
+    public static ArrayList<Card> buildHand() {
+    }
+
+    private boolean getHighlight() {
+    }
---
+++ src/DrawPanel.java
@@ -0,0 +1,105 @@
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.awt.Font;
+import java.util.List;
+
+class DrawPanel extends JPanel implements MouseListener {
+
+    private ArrayList<Card> hand;
+    private ArrayList<Card> deck;
+    private Rectangle button;
+    private Rectangle replace;
+    private boolean LoseMessage = false;
+
+    public DrawPanel() {
+        button = new Rectangle(147, 100, 160, 26);
+        replace = new Rectangle(330, 25, 160, 26);
+        deck = Card.buildDeck();
+        this.addMouseListener(this);
+        hand = Card.buildHand();
+        System.out.println(hand);
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int x = 50;
+        int y = 10;
+
+        int cardNumber = 0;
+        int temp = 0;
+
+        for (int i = 0; i < hand.size(); i++) {
+            Card c = hand.get(i);
+            if (c.getHighlight()) {
+                // draws the border around the card
+                g.drawRect(x, y, c.getImage().getWidth(), c.getImage().getHeight());
+            }
+            c.setRectangleLocation(x, y);
+            g.drawImage(c.getImage(), x, y, null);
+
+            x = x + c.getImage().getWidth() + 10;
+            if ((cardNumber + 1) % 3 == 0) {
+                y = y + c.getImage().getHeight() + 10;
+                x = 120;
+            }
+            cardNumber++;
+        }
+        g.setFont(new Font("Courier New", Font.BOLD, 20));
+        g.drawString("PLAY AGAIN", 165, 270);
+        g.drawString("REPLACE CARDS", 333, 45);
+        g.drawString("Cards Left: " + deck.size(), 30, 450);
+        if (LoseMessage) {
+            g.drawString("No available moves! GAME OVER!", 30, 350);
+        }
+        g.drawRect((int)button.getX(), (int)button.getY(), (int)button.getWidth(), (int)button.getHeight());
+        g.drawRect((int)replace.getX(), (int)replace.getY(), (int)replace.getWidth(), (int)replace.getHeight());
+    }
+
+    public void mousePressed(MouseEvent e) {
+
+        Point clicked = e.getPoint();
+
+        if (e.getButton() == 1) {
+            if (button.contains(clicked)) {
+                hand = Card.buildHand();
+                deck = Card.buildDeck();
+                LoseMessage = false;
+            }
+
+            for (int i = 0; i < hand.size(); i++) {
+                Rectangle box = hand.get(i).getCardBox();
+                if (box.contains(clicked)) {
+                    hand.get(i).flipCard();
+                }
+            }
+
+            if (replace.contains(clicked)) {
+                deck = Card.checkDeck(deck, hand);
+                System.out.println(deck);
+                if (Card.canEliminate(hand)) {
+                    for (int i = 0; i < hand.size(); i++) {
+                        if (hand.get(i).getHighlight()) {
+                            System.out.println(hand.get(i).getValue());
+                            Card.replaceCard(deck, hand, i);
+                        }
+                    }
+                }
+
+                if (!Card.movesLeft(hand)) {
+                    LoseMessage = true;
+                }
+                deck = Card.checkDeck(deck, hand);
+                System.out.println(LoseMessage);
+            }
+        }
+    }
+    public void mouseReleased(MouseEvent e) { }
+    public void mouseEntered(MouseEvent e) { }
+    public void mouseExited(MouseEvent e) { }
+    public void mouseClicked(MouseEvent e) { }
+}
